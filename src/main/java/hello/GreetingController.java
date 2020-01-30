52e492d795c4b7b10b48d7bf475ae149cfc516bd
tsharon-byte <sharonova_t@inbox.ru>
2020-01-30 15:18:19 +0300

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homeGet(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
